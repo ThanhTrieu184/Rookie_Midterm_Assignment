334f2bab17e8eef1d38bc60ac368d5ad424491a9
Huỳnh Triều <huynhthanhtrieu184@gmail.com>
2021-07-02 10:02:21 +0700

    initial bookworm app

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index() {
+        $books = DB::table('books')->get();
+        foreach ($books as $book) {
+            echo '<h2>'.$book->book_price.'</h2><br>';
+        }
+        // return view("/hello",compact('users'));
+    }
+}
